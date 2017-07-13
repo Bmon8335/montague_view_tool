ce6c9866a51135f8190640276210e9b9908f4aeb
Brody Montague <montaguebrody@gmail.com>
2017-07-13 15:37:32 +0000

    Dev tool initial commit

---
+++ lib/montague_view_tool.rb
@@ -0,0 +1,6 @@
+require "montague_view_tool/version"
+require "montague_view_tool/renderer"
+
+module MontagueViewTool
+  # Your code goes here...
+end
---
+++ lib/montague_view_tool/version.rb
@@ -0,0 +1,3 @@
+module MontagueViewTool
+  VERSION = "0.1.0"
+end
